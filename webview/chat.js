40830a07b9a530686c75292d0f84deecab69d50b
Kroos Xiang <kroosvi@gmail.com>
2023-11-10 11:59:22 +0800

    css and html updated

--- webview/chat.js
+++ webview/chat.js
@@ -7,7 +7,7 @@ const chatInputForm = document.querySelector('.chat_input_form');
 const chatInput = document.querySelector('.user_input');
 
 const createChatMessageElement = (message) => `
-    <div class="message_${message.sender === 'user' ? 'user' : 'chatGPT'}">${message.text}</div>
+    <div class="message message_${message.sender === 'user' ? 'user' : 'chatGPT'}">${message.text}</div>
 `;
 
 const sendMessage = (e) =>{
