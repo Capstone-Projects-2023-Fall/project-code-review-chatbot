6114860841b208c76e35fc433e8470701832e7a7
yoonjaejasonlee <tug95015@temple.edu>
2023-11-30 18:19:57 -0500

    controller fix

--- server/code-review-chatbot/app/Http/Controllers/DBLogging.php
+++ server/code-review-chatbot/app/Http/Controllers/DBLogging.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
-class YourController extends Controller
+class DBLogging extends Controller
 {
     public function logData(Request $request)
     {
@@ -24,14 +24,15 @@ class YourController extends Controller
             'logs' => $request->log,
             'hash' => $request->hash,
             'gitdiff' => $request->gitdiff,
-            'num_tokens' => $request->token
+            'num_token' => $request->token
         ];
 
         try {
-            DB::table('test')->insert($checkboxData);
+            DB::table('log_data')->insert($checkboxData);
             return response()->json(['message' => 'Successfully inserted data'], 200);
         } catch (\Exception $e) {
-            return response()->json(['error' => 'Error inserting data'], 500);
+            \Log::error($e->getMessage());
+            return response()->json(['error' => 'Error inserting data', 'details' => $e->getMessage()], 500);
         }
     }
 }
