4bf982855beb3921a4ec473a79e2192ef2b274e1
yoonjaejasonlee <tug95015@temple.edu>
2023-11-30 13:34:32 -0500

    added api endpoint for logging

--- server/code-review-chatbot/routes/api.php
+++ server/code-review-chatbot/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use OpenAI\Laravel\Facades\OpenAI;
+use App\Http\Controllers\DBLogging;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use OpenAI\Laravel\Facades\OpenAI;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('/log', [DBLogging::class, 'logData']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
