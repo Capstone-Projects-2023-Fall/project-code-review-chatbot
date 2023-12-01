c2febef553e6ff5d57e22f272cb3ea2c1393c1b8
Kroos Xiang <kroosvi@gmail.com>
2023-12-02 00:09:12 +0800

    change deleted from the thing

--- media/scripts/chat.js
+++ media/scripts/chat.js
@@ -1,6 +1,3 @@
-//buttons
-const enterButton = document.querySelector('.enter_button');
-
 //Message area
 const chatMessages = document.querySelector('.chatbox_message');
 const chatInputForm = document.querySelector('.chat_input_form');
@@ -9,24 +6,6 @@ const chatInput = document.querySelector('.user_input');
 const createChatMessageElement = (message) => `
     <div class="message message_${message.sender === 'user' ? 'user' : 'chatGPT'}">${message.text}</div>
 `;
-
-const sendMessage = (e) =>{
-    e.preventDefault();
-
-    const message = {
-        sender : 'user',
-        text:chatInput.value,
-    };
-
-    //update the html
-    chatMessages.innerHTML += createChatMessageElement(message);
-
-    //reset the inputbox
-    chatInputForm.reset();
-};
-
-//waiting for the user to click the send button
-chatInputForm.addEventListener('submit',sendMessage);
 
 // @ts-ignore 
 
