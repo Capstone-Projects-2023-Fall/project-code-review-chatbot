7fcda65f6764caee96e2b58299ad7e0326166b21
PatrickBrady7 <97626904+PatrickBrady7@users.noreply.github.com>
2023-12-02 20:08:41 -0500

    emailVerificationSetup
    email verification is now required, changed code so that it does not automatically send users an email upon signing up.

--- server/code-review-chatbot/resources/views/auth/verify-email.blade.php
+++ server/code-review-chatbot/resources/views/auth/verify-email.blade.php
@@ -5,26 +5,10 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            {{ __('Before continuing, your email address will need to be verified. An email will be sent after manual approval from an admin. Please allow some time as an admin must manually approve users.') }}
         </div>
 
-        @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                {{ __('A new verification link has been sent to the email address you provided in your profile settings.') }}
-            </div>
-        @endif
-
         <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
-                @csrf
-
-                <div>
-                    <x-button type="submit">
-                        {{ __('Resend Verification Email') }}
-                    </x-button>
-                </div>
-            </form>
-
             <div>
                 <a
                     href="{{ route('profile.show') }}"
