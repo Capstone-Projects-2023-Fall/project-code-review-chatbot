a0aab691b4c4442199a996fbf9715498eb8f7119
PatrickBrady7 <97626904+PatrickBrady7@users.noreply.github.com>
2023-12-03 03:54:50 -0500

    verifyFunc
    moved verifyAUser function into the admin controller

--- server/code-review-chatbot/app/Http/Controllers/AdminController.php
+++ server/code-review-chatbot/app/Http/Controllers/AdminController.php
@@ -13,4 +13,16 @@ class AdminController extends Controller
         $users = User::all(); // Fetch all users
         return view('admin-dashboard', compact('users'));
     }
+
+    //function to verify a certian user
+    public function verifyAUser(Request $request) {
+        $validator = Validator::make($request->all(), [ 
+            'email' => 'required|email', 
+        ]);
+        if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);            
+        }
+        $emailForValidation = $request;
+    }
+
 }
