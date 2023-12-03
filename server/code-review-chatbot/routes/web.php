0fb6bda4904596c2412b01c4c766516d5735c4ab
PatrickBrady7 <97626904+PatrickBrady7@users.noreply.github.com>
2023-12-03 14:14:59 -0500

    verification
    Added routes and requirement for users to verify their email.

--- server/code-review-chatbot/routes/web.php
+++ server/code-review-chatbot/routes/web.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\AdminController;
 
 use App\Http\Controllers\UserController;
 
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -75,6 +78,22 @@ Route::get('/authorize', function(Request $request) {
 
     
 });
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+ 
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 
 
