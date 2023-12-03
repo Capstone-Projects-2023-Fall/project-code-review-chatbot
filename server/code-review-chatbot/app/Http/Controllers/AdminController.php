38eb2fdd218aa074d33b1e0e0501914fc7017e87
PatrickBrady7 <97626904+PatrickBrady7@users.noreply.github.com>
2023-12-02 20:42:19 -0500

    verifyFunction
    Created a function that asks for an email so that a verification request can be sent to that user. Will be implemented in the admin panel

--- server/code-review-chatbot/app/Http/Controllers/AdminController.php
+++ server/code-review-chatbot/app/Http/Controllers/AdminController.php
@@ -11,4 +11,17 @@ use Illuminate\Routing\Controller as BaseController;
 class AdminController extends Controller
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    //function to verify a certian user
+    public function verifyAUser(Request $request) {
+        $validator = Validator::make($request->all(), [ 
+            'email' => 'required|email', 
+        ]);
+        if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);            
+        }
+        $emailForValidation = $request;
+        $userToBeValidated = User::where('email', $emailForValidation);
+    }
+
 }
